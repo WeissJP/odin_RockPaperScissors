67ccaec7d1c9c53a12dba352e395464b3b1f5769
Joe Weiss <jpwracing.99@gmail.com>
2023-09-22 19:41:20 -0500

    Add function for computer's random choice

---
+++ script.js
@@ -0,0 +1,12 @@
+function getComputerChoice() {
+    let computerChoice;
+    let randomNumber = Math.floor(Math.random() * 100) + 1;
+    
+    if (randomNumber >= 0 && randomNumber <= 33) {
+        computerChoice = "Rock";
+    } else if (randomNumber > 33 && randomNumber <= 66) {
+        computerChoice = "Paper";
+    } else {
+        computerChoice = "Scissors";
+    }
+}
