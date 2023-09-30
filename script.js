8b19a228676f6472dbabc2b0b2567b5b9bf73bad
Joe Weiss <jpwracing.99@gmail.com>
2023-09-30 18:36:41 -0500

    Rework cpu choice & add func to loop game 5x

--- script.js
+++ script.js
@@ -1,49 +1,59 @@
-function getComputerChoice() {
-    let computerChoice;
-    let randomNumber = Math.floor(Math.random() * 100) + 1;
+let playerSelection;
+let computerSelection;
+
+function getComputerChoice(computerChoice) {
+    let randomNumber = Math.floor(Math.random() * 3);
     
-    if (randomNumber >= 0 && randomNumber <= 33) {
+    if (randomNumber === 0) {
         computerChoice = "rock";
-    } else if (randomNumber > 33 && randomNumber <= 66) {
+    } else if (randomNumber === 1) {
         computerChoice = "paper";
-    } else {
+    } else if (randomNumber === 2) {
         computerChoice = "scissors";
     }
-    
     return computerChoice;
 }
 
 function playSingleRound(playerSelection, computerSelection) {
+    playerSelection = prompt("What is your selection?").toLowerCase();
+    computerSelection = getComputerChoice();
+    
+    let result;
+    
     // Logic for player choosing paper
     if ((playerSelection === "paper") && (computerSelection === "rock")) {
-        alert("You won! Paper beats rock.");
+        result = "You won! Paper beats rock.";
     } else if ((playerSelection === "paper") && (computerSelection === "scissors")) {
-        alert("You lose! Scissors beats paper.");
+        result = "You lose! Scissors beats paper.";
     } else if ((playerSelection === "paper") && (computerSelection === "paper")) {
-        alert("You tied! Try again.");
+        result = "You tied! Try again.";
     }
 
     // Logic for player choosing scissors
     if ((playerSelection === "scissors") && (computerSelection === "paper")) {
-        alert("You won! Scissors beats paper.");
+        result = "You won! Scissors beats paper.";
     } else if ((playerSelection === "scissors") && (computerSelection === "rock")) {
-        alert("You lose! Rock beats scissors.");
+        result = "You lose! Rock beats scissors.";
     } else if ((playerSelection === "scissors") && (computerSelection === "scissors")) {
-        alert("You tied! Try again.");
+        result = "You tied! Try again.";
     }
 
     // Logic for player choosing rock
     if ((playerSelection === "rock") && (computerSelection === "scissors")) {
-        alert("You won! Rock beats scissors");
+        result = "You won! Rock beats scissors";
     } else if ((playerSelection === "rock") && (computerSelection === "paper")) {
-        alert("You lose! Paper beats rock.");
+        result = "You lose! Paper beats rock.";
     } else if ((playerSelection === "rock") && (computerSelection === "rock")) {
-        alert("You tied! Try again.");
+        result = "You tied! Try again.";
     }
+    console.log(result);
 
 }
 
+function game() {
+    for (let i = 0; i < 5; i++) {
+        playSingleRound();
+    }
+}
 
-let playerSelection = prompt("What is your selection?").toLowerCase();
-let computerSelection = getComputerChoice();
-playSingleRound(playerSelection, computerSelection);
+game();
